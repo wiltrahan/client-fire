1b39d1f7f7219dd840ffb1d8da923ba1491551e4
wiltrahan <twilorip@gmail.com>
2018-12-11 19:49:08 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientsTableComponent } from './clients-table/clients-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
