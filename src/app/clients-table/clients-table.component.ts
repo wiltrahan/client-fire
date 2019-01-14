f2d2389668243de89bbd2556c2a90a64bd387cc5
wiltrahan <twilorip@gmail.com>
2019-01-14 17:12:24 -0500

    setup table/update ignore

--- src/app/clients-table/clients-table.component.ts
+++ src/app/clients-table/clients-table.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { DataService } from './../core/data.service';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-clients-table',
   templateUrl: './clients-table.component.html',
   styleUrls: ['./clients-table.component.scss']
 })
-export class ClientsTableComponent implements OnInit {
+export class ClientsTableComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  displayedColumns = ['firstName', 'lastName', 'phone'];
+  dataSource = new MatTableDataSource();
+  constructor(private dataService: DataService) {}
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngOnInit() {
+    // this.dataService.getClients().subscribe(
+    //   data => {
+    //     this.dataSource.data = data;
+    //   }
+    // );
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
