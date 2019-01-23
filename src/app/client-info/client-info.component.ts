b79f94557d3ce4f286fd3248d06f0e807036d5cb
wiltrahan <twilorip@gmail.com>
2019-01-23 16:23:55 -0500

    parse time to am/pm

--- src/app/client-info/client-info.component.ts
+++ src/app/client-info/client-info.component.ts
@@ -28,8 +28,28 @@ export class ClientInfoComponent implements OnInit, OnDestroy {
   getClient(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.clientSubscription = this.dataService.getClient(id).subscribe(data => {
-      this.client = data;   
+      this.client = data;
+      this.client.startTime = this.convertTimeString(data.startTime);
+      this.client.endTime = this.convertTimeString(data.endTime);
     });
+  }
+
+  convertTimeString(time: string) {
+    const hour = time.substring(0, 2);
+    const minute = time.substring(2);
+    const am = 'am';
+    const pm = 'pm';
+    
+    if(hour[0] === '0') {
+      time = (parseInt(hour[1], 10) % 12).toString() + minute + am;
+    } else if(hour[0] === '1' && hour[1] === '0') {
+      time = '10' + minute + am;
+    } else if(hour[0] === '1' && hour[1] === '1') {
+      time = '11' + minute + am;
+    } else {
+      time = (parseInt(hour, 10) % 12).toString() + minute + pm;
+    }
+    return time;
   }
 
   goBack(): void {
